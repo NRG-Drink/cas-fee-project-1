b5631cf07d4fbbc15cfdd29475155ea04f798145
NRG-Drink <beatdz93@hotmail.com>
2026-06-07 11:44:19 +0200

    test: add config to generate test coverage reports

---
+++ vitest.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    coverage: {
+      all: true,
+      provider: 'v8', // Use 'v8' for built-in coverage
+      reporter: ['text', 'json', 'html'], // Formats for the coverage report
+      include: ['code/public/scripts/**/*'], // Include files for coverage
+      exclude: ['node_modules', 'code/public/scripts/index.js'], // Exclude specific files
+    },
+  },
+});
